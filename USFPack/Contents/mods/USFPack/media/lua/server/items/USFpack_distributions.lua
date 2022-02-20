c2be65bca569098568e905010ce3558f949a09f5
Roman Almazov <ralmaz.vue@ya.ru>
2022-02-20 17:45:21 +0200

    fix: spawn points

--- USFPack/Contents/mods/USFPack/media/lua/server/items/USFpack_distributions.lua
+++ USFPack/Contents/mods/USFPack/media/lua/server/items/USFpack_distributions.lua
@@ -3,25 +3,25 @@ require 'Items/SuburbsDistributions'
 -- Add items for Police
 table.insert(SuburbsDistributions["policestorage"]["all"].items, "USFpack.USFbackpack");
 table.insert(SuburbsDistributions["policestorage"]["all"].items, 4);
--- table.insert(SuburbsDistributions["policestorage"]["metal_shelves"].items, "USFpack.USFbackpack");
--- table.insert(SuburbsDistributions["policestorage"]["metal_shelves"].items, 4);
--- table.insert(SuburbsDistributions["storageunit"]["crate"].items, "USFpack.USFbackpack");
--- table.insert(SuburbsDistributions["storageunit"]["crate"].items, 4);
+table.insert(SuburbsDistributions["policestorage"]["metal_shelves"].items, "USFpack.USFbackpack");
+table.insert(SuburbsDistributions["policestorage"]["metal_shelves"].items, 4);
+table.insert(SuburbsDistributions["storageunit"]["crate"].items, "USFpack.USFbackpack");
+table.insert(SuburbsDistributions["storageunit"]["crate"].items, 4);
 
 -- Add items for Gun store
--- table.insert(SuburbsDistributions["gunstore"]["locker"].items, "USFpack.USFbackpack");
--- table.insert(SuburbsDistributions["gunstore"]["locker"].items, 4);
+table.insert(SuburbsDistributions["gunstore"]["locker"].items, "USFpack.USFbackpack");
+table.insert(SuburbsDistributions["gunstore"]["locker"].items, 4);
 table.insert(SuburbsDistributions["gunstore"]["all"].items, "USFpack.USFbackpack");
 table.insert(SuburbsDistributions["gunstore"]["all"].items, 4);
--- table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, "USFpack.USFbackpack");
--- table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, 4);
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, "USFpack.USFbackpack");
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, 4);
 
 -- Add items Hunting home
--- table.insert(SuburbsDistributions["hunting"]["all"].items, "USFpack.USFbackpack");
--- table.insert(SuburbsDistributions["hunting"]["all"].items, 4);
+table.insert(SuburbsDistributions["hunting"]["all"].items, "USFpack.USFbackpack");
+table.insert(SuburbsDistributions["hunting"]["all"].items, 4);
 
 -- All
--- table.insert(SuburbsDistributions["all"]["crate"].items, "USFpack.USFbackpack");
--- table.insert(SuburbsDistributions["all"]["crate"].items, 4);
--- table.insert(SuburbsDistributions["all"]["metal_shelves"].items, "USFpack.USFbackpack");
--- table.insert(SuburbsDistributions["all"]["metal_shelves"].items, 4);
+table.insert(SuburbsDistributions["all"]["crate"].items, "USFpack.USFbackpack");
+table.insert(SuburbsDistributions["all"]["crate"].items, 4);
+table.insert(SuburbsDistributions["all"]["metal_shelves"].items, "USFpack.USFbackpack");
+table.insert(SuburbsDistributions["all"]["metal_shelves"].items, 4);
